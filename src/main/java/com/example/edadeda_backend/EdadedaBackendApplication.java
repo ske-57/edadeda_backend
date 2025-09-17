53c4a790939e49e1f8c3aec640666c55ab78714e
Arseniy_Kuzenkov <senya57k@gmail.com>
2025-09-17 03:20:48 +0300

    Initialize dependencies: boot-web, data-jpa, postgresql, lombok, starter-test

---
+++ src/main/java/com/example/edadeda_backend/EdadedaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.edadeda_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdadedaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdadedaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/edadeda_backend/EdadedaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.edadeda_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdadedaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
